ef4894f5dd88164bd26292b96e4c48b44a73b2fb
Sergio Rezende <sergiorezende2001@gmail.com>
2025-04-20 21:24:28 -0300

    feat: implement game logic and styling for tiles in the word guessing game

--- src/app/components/line/line.component.ts
+++ src/app/components/line/line.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, computed, input, InputSignal } from '@angular/core';
 import { Guess } from '../../models/guess';
-import { Tile } from '../../models/tile';
+import { GameService } from '../../services/game.service';
 
 @Component({
     selector: 'app-line',
@@ -13,44 +13,12 @@ export class LineComponent {
     guess: InputSignal<Guess> = input.required();
     solution: InputSignal<string> = input.required();
 
+    constructor(private readonly gameService: GameService) {}
+
     tiles = computed(() => {
         const guess = this.guess();
         const solution = this.solution();
-        const tiles = new Array<Tile>(solution.length);
 
-        const isMarkedArray = new Array<boolean>(solution.length).fill(false);
-
-        for (let i = 0; i < solution.length; i++) {
-            tiles[i] = { value: guess.value[i] || '' };
-
-            if (guess.isConfirmed) {
-                if (guess.value[i] === solution[i]) {
-                    tiles[i].status = 'correct';
-                    isMarkedArray[i] = true;
-                    continue;
-                }
-
-                if (!solution.includes(guess.value[i])) {
-                    tiles[i].status = 'absent';
-                    continue;
-                }
-
-                const indexLetter = solution
-                    .split('')
-                    .findIndex(
-                        (letter, index) =>
-                            letter === guess.value[i] && !isMarkedArray[index]
-                    );
-                if (indexLetter >= 0) {
-                    tiles[i].status = 'present';
-                    isMarkedArray[indexLetter] = true;
-                    continue;
-                }
-
-                tiles[i].status = 'absent';
-            }
-        }
-
-        return tiles;
+        return this.gameService.getTiles(guess, solution);
     });
 }
--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,12 +1,49 @@
 import { Injectable } from '@angular/core';
+import { Tile } from '../models/tile';
+import { Guess } from '../models/guess';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class GameService {
-  readonly NUMBER_OF_GUESSES = 6;
-  readonly NUMBER_OF_LETTERS = 5;
+    readonly NUMBER_OF_GUESSES = 6;
+    readonly NUMBER_OF_LETTERS = 5;
 
-  constructor() { }
-  
+    getTiles(guess: Guess, solution: string): Tile[] {
+        const tiles = new Array<Tile>(solution.length)
+            .fill({ value: '' })
+            .map((_, index) => {
+                return { value: guess.value[index] } as Tile;
+            });
+
+        if (!guess.isConfirmed) {
+            return tiles;
+        }
+
+        const isMarkedArray = new Array<boolean>(solution.length).fill(false);
+
+        for (let [i, tile] of tiles.entries()) {
+            if (tile.value === solution[i]) {
+                tiles[i].status = 'correct';
+                isMarkedArray[i] = true;
+            }
+        }
+
+        for (let [i, tile] of tiles.entries()) {
+            const indexLetter = solution
+                .split('')
+                .findIndex(
+                    (letter, index) =>
+                        letter === tile.value && !isMarkedArray[index]
+                );
+            if (indexLetter >= 0) {
+                tiles[i].status = 'present';
+                isMarkedArray[indexLetter] = true;
+            }
+
+            tiles[i].status = tiles[i].status ?? 'absent';
+        }
+
+        return tiles;
+    }
 }
